e73f3e1af35d824e8c02477499d900bdb2cfa720
REFAT SHAHRIAR <109792845+REFAT-156@users.noreply.github.com>
2023-01-03 01:00:15 +0600

    Update Rndm.py

--- Rndm.py
+++ Rndm.py
@@ -16,7 +16,7 @@ bit = platform.architecture()[0]
  
 if bit == "64bit":
  
-        from rndm import Main
+        from refat import Main
  
         Main()
  
@@ -24,7 +24,7 @@ if bit == "64bit":
  
 elif bit == "32bit":
  
-        from joss import Main
+        from refat import Main
  
  
         Main()
