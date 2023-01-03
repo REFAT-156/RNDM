4d780aafa155e98b1c6e653688803f91fd83ab8f
REFAT SHAHRIAR <109792845+REFAT-156@users.noreply.github.com>
2023-01-03 22:50:18 +0600

    Update Rndm.py

--- Rndm.py
+++ Rndm.py
@@ -1,21 +1,21 @@
 import os, platform, time
-print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
+print('\n\x1b[1;37m[●] Checking Update.....');time.sleep(0.5)
 def Update():
-    exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
+    exit('\033[1;31m[●] Commands On Update Please Wait For Update ❤ ')
 def Run():
         bit = platform.architecture()[0]
         if bit == '64bit':
-            print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
-            print('[•] Follow My Github First')
+            print("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
+            print('[●] Follow My Github First')
             os.system('xdg-open https://github.com/REFAT-156')
             from refat import Main
             Main()
         elif bit == '32bit':
-            print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
-            print('[•] Follow My Github First')
+            print("\n\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
+            print('[●] Follow My Github First')
             os.system('xdg-open https://github.com/REFAT-156')
             from refat import Main
             Main()
         else:
-            exit('\033[1;31m[×] Connection Error')
+            exit('\033[1;31m[●] Connection & Network Error')
 Update()
