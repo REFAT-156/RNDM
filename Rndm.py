ca45654fa65c7e76e96f6fcb1a0906502dd82527
REFAT SHAHRIAR <109792845+REFAT-156@users.noreply.github.com>
2023-02-04 21:14:27 +0600

    Update Rndm.py

--- Rndm.py
+++ Rndm.py
@@ -1,21 +1,27 @@
-import os, platform, time
-print('\n\x1b[1;37m[●] Checking Update.....');time.sleep(0.5)
+import os, platform, time, sys
+def xoss(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.02)
+xoss('\n\x1b[1;37m[●] Checking Update........✔️✔️');time.sleep(0.5)
+os.system("git pull")
 def Update():
     exit('\033[1;31m[●] Commands On Update Please Wait For Update ❤ ')
 def Run():
         bit = platform.architecture()[0]
         if bit == '64bit':
-            print("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
-            print('[●] Follow My Github First')
-            os.system('xdg-open https://github.com/REFAT-156')
-            from refat import Main
-            Main()
+            xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🍼🙂")
+            xoss('\x1b[1;94m[●] Join My Group First 🎈')
+            os.system('xdg-open https://facebook.com/groups/1431748223768752/')
+            from refat import welcome
+            welcome()
         elif bit == '32bit':
-            print("\n\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
-            print('[●] Follow My Github First')
+            xoss("\n\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🍼🙂")
+            xoss('\x1b[1;94m[●] Follow My Github First 🎈')
             os.system('xdg-open https://github.com/REFAT-156')
-            from refat import Main
-            Main()
+            from refat import welcome
+            welcome()
         else:
-            exit('\033[1;31m[●] Connection & Network Error')
+            exit('\033[1;31m[●] Connection & Network Error 🤕')
 Run()
